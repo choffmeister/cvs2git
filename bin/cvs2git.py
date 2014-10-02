1a6ec7f68ba4567590e4422271a545ae81d87218
Christian Hoffmeister <mail@choffmeister.de>
2014-10-02 09:37:08 +0200

    Use cvs2git config file internally

--- bin/cvs2git.py
+++ bin/cvs2git.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python
+# (Be in -*- mode: python; coding: utf-8 -*- mode.)
 
 import os
 import sys
 import subprocess
 import tempfile
+import codecs
+
+base_path = os.path.dirname(os.path.realpath('%s/..' % __file__))
+
+def generate_config_file(tmpl_path, target_path, values, author_map):
+  with codecs.open(tmpl_path, 'r', 'utf-8') as f: conf = f.read()
+  for key in values: conf = conf.replace('$%s$' % key, values[key])
+  author_str = "\n    ".join(map(lambda a: "'%s': (u'%s', '%s')," % (a, author_map[a][0], author_map[a][1]), author_map))
+  conf = conf.replace('$author_map$', author_str)
+  with codecs.open(target_path, 'w', 'utf-8') as f: f.write(conf)
 
 def execute(cmd, cwd = None):
   subprocess.Popen(cmd, cwd=cwd).communicate()
 
-def cvs2git(cvs_path, git_path):
+def cvs2git(cvs_path, git_path, author_map):
+  global base_path
   temp_blob_path = tempfile.mkstemp('-blob.dat')[1]
   temp_dump_path = tempfile.mkstemp('-dump.dat')[1]
+  temp_conf_path = tempfile.mkstemp('-config.py')[1]
+
+  generate_config_file('%s/config.tmpl.py' % base_path, temp_conf_path, {
+    'blob_path': temp_blob_path,
+    'dump_path': temp_dump_path,
+    'cvs_path': cvs_path,
+  }, author_map)
 
   print 'Removing Attic duplicates'
   files = []
@@ -26,13 +45,7 @@ def cvs2git(cvs_path, git_path):
         os.remove(attic)
 
   print 'Running cvs2git'
-  execute([
-    'cvs2git',
-    '--blobfile', temp_blob_path,
-    '--dumpfile', temp_dump_path,
-    '--username', 'chhoffme',
-    '--encoding', 'windows-1252',
-    cvs_path])
+  execute(['cvs2git', '--options=%s' % temp_conf_path])
 
   print 'Importing to GIT repository'
   os.makedirs(git_path)
@@ -55,4 +68,7 @@ def cvs2git(cvs_path, git_path):
 if len(sys.argv) != 3:
   print 'usage: migrate.py /path/to/cvs/dir /path/to/git/dir'
 else:
-  cvs2git(sys.argv[1], sys.argv[2])
+  author_map = {
+    'user1': (u'First User', 'first.user@company.com'),
+  }
+  cvs2git(sys.argv[1], sys.argv[2], author_map)
